af3dd0050a0c953cca97debc4edc69884a1de89c
Juan Pablo Sotelo <makonethekillah@gmail.com>
2019-05-15 09:32:31 -0500

    se quita minificado de dist

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,13 @@
-"use strict";!function(c){c(".search").keyup(function(){var n=c(this).val();c(".block").unmark({done:function(){c(".block").mark(n)}})}),c(".toggle").click(function(){c(".acordeon-content").toggleClass("hidden")})}(jQuery);
+"use strict";
+! function(c) {
+  c(".search").keyup(function() {
+    var n = c(this).val();
+    c(".block").unmark({
+      done: function() {
+        c(".block").mark(n)
+      }
+    })
+  }), c(".toggle").click(function() {
+    c(".acordeon-content").toggleClass("hidden")
+  })
+}(jQuery);
