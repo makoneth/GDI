78ce625812297c943c6e5f06c9ebe0aa12faa5fa
Juan Pablo Sotelo <makonethekillah@gmail.com>
2019-05-15 09:04:51 -0500

    Dist por si el equipo tiene problemas al generarlo
    Se sube DIST por si no pueden instalar los paquetes o si no quieren complicarse la vida

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";!function(c){c(".search").keyup(function(){var n=c(this).val();c(".block").unmark({done:function(){c(".block").mark(n)}})}),c(".toggle").click(function(){c(".acordeon-content").toggleClass("hidden")})}(jQuery);
